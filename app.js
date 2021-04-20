903de039a96646cf1f27ed1de26a8baacfb7d03b
Petra Boldi <boldi.petra@gmail.com>
2021-04-20 18:06:21 +0200

    user story 1

--- app.js
+++ app.js
@@ -5,13 +5,21 @@ const port = 3000;
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/", (req, res) => {
 	res.render("./index");
 });
 
-// app.post("/", (req, res) => {
-// 	res.render("./index", {});
-// });
+app.post("/", (req, res) => {
+	console.log(req.body)
+	console.log(req.body['first-person-name'])
+	console.log(req.body['second-person-name'])
+	res.render("./battle", 
+	{firstPersonName:req.body['first-person-name'],
+	secondPersonName:req.body['second-person-name']
+})});
+
 
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`);
--- cypress/integration/battle.spec.js
+++ cypress/integration/battle.spec.js
@@ -6,4 +6,12 @@ describe("battle feature test", function () {
 	it("shows a heading for the game", function () {
 		cy.contains("Get ready to battle!");
 	});
+		it("shows an input field for providing names", function () {
+		cy.get('#first-player').type('Marie')
+		cy.get('#second-player').type('Petra')
+		cy.get('#submit-button').click()
+		cy.contains('The first player is Marie!')
+		cy.contains('The second player is Petra!')
+	});
+
 });
